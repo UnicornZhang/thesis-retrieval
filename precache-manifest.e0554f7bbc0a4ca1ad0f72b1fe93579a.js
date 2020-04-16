c336057226a441b65a2e5471573ef3d6d0fc3a99
zhangwei <zhangwei@xique.com>
2020-04-16 10:06:36 +0800

    Updates

---
+++ precache-manifest.e0554f7bbc0a4ca1ad0f72b1fe93579a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ad6d413234bac46763d84684d5f69e",
+    "url": "/thesis-retrieval/index.html"
+  },
+  {
+    "revision": "50a2de8c04c87d0b2ea3",
+    "url": "/thesis-retrieval/static/css/2.8b9164f1.chunk.css"
+  },
+  {
+    "revision": "a105b7f82b463dbf3dc7",
+    "url": "/thesis-retrieval/static/css/main.b87e9c13.chunk.css"
+  },
+  {
+    "revision": "50a2de8c04c87d0b2ea3",
+    "url": "/thesis-retrieval/static/js/2.144b6d7c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/thesis-retrieval/static/js/2.144b6d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a105b7f82b463dbf3dc7",
+    "url": "/thesis-retrieval/static/js/main.6364c16a.chunk.js"
+  },
+  {
+    "revision": "9088e6be8b7b9786f68a",
+    "url": "/thesis-retrieval/static/js/runtime-main.e6519581.js"
+  },
+  {
+    "revision": "4d5995f4b5e52006758baca70e5ebce8",
+    "url": "/thesis-retrieval/static/media/bg.4d5995f4.jpeg"
+  },
+  {
+    "revision": "7cff46f464da25d4db1e94f3ae5d7f98",
+    "url": "/thesis-retrieval/static/media/img.7cff46f4.jpg"
+  }
+]);
